2f4eed695840c41831d538f662c8bc5d86b506f1
willy-5354 <121175540+willy-5354@users.noreply.github.com>
2024-04-11 16:50:12 +0200

    orologio_js

--- orologio_di_berlino/assets/scripts/app.js
+++ orologio_di_berlino/assets/scripts/app.js
@@ -1,1 +1,21 @@
-console.log("ciao");
+setInterval(() => {
+    // variabili
+    let hour = document.getElementById("hour");
+    let minute = document.getElementById("minute");
+    let second = document.getElementById("second");
+
+    // ogetto per date
+    let date = new Date();
+    // method
+    let = date.getHours();
+    let = date.getMinutes();
+    let = date.getSeconds();
+
+    let hr_rotate = 30 * hr + min /2; //converting current time
+    let mn_rotate = 6 * min;
+    let sec_rotate = sec;
+
+    hour.style.transform = `rotate(${hr_rotate}deg)`;
+    minute.style.transform = `rotate(${mn_rotate}deg)`;
+    second.style.transform = `rotate(${sec_rotate}deg)`;
+},1000);
